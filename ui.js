63e959279ad4c55385458676b51455ca32514ae7
omid nassir <nassiromid@gmail.com>
2020-05-28 10:40:31 -0700

    Render profile picture and link

--- ui.js
+++ ui.js
@@ -4,7 +4,28 @@ class UI{
     }
 
     showProfile(user){
-        console.log(user);
+        this.profile.innerHTML = `
+        <div class="card card-body mb-3">
+            <div class="row">
+                <div class="col-md-3">
+                    <img src="${user.avatar_url}" class="img-fluid mb-2">
+                    <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block">View Profile</a>
+                </div>
+                <div class="col-md-9">
+
+                </div>
+
+
+            </div>
+    </div>
+        
+        
+        
+        
+        
+        
+        
+        `;
         
     }
 }
